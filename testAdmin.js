390b4d23104936cd3f112753e3ddcd88d5492d94
tilahundiriba <Tilishdiriba1424@gmail.com>
2023-05-20 18:04:09 +0300

    updated

---
+++ testAdmin.js
@@ -0,0 +1,15 @@
+
+
+
+
+function getvalue() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    document.getElementById('un').innerHTML = username;
+    document.getElementById('pw').innerHTML = password;
+}
+
+
+
+
+
